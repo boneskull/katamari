dd1acf270768bae7715807e5621793a2bb5eec55
Christopher Hiller <boneskull@boneskull.com>
2017-04-13 22:47:22 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const hapi = require('hapi');
+const githubWebhooksPlugin = require('hapi-github-webhooks');
+const token = process.env.WEBHOOK_SECRET;
+const server = new hapi.Server();
+ 
+server.connection({
+    host: host,
+    port: port
+});
+ 
+server.register(githubWebhooksPlugin, function (err) {
+  // Register github webhook auth strategy 
+  server.auth.strategy('githubwebhook', 'githubwebhook', { secret: token});
+  // Apply the strategy to the route that handles webhooks 
+  server.route([
+    {
+      method: 'POST',
+      path: '/webhooks/github',
+      config: {
+          auth: {
+              strategies: ["githubwebhook"],
+              payload: 'required'
+          }
+      },
+      handler: function(request, reply) {
+        // request.payload is the validated payload from GitHub
+        console.log(request.payload);
+        reply();
+      }
+    }
+  ]);
+});
