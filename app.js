7421b8677984eac1e654e88819b3ca63ffeff994
Christopher Hiller <boneskull@boneskull.com>
2017-04-13 22:53:11 -0700

    fix port?

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const hapi = require('hapi');
 const githubWebhooksPlugin = require('hapi-github-webhooks');
 const token = process.env.WEBHOOK_SECRET;
 const server = new hapi.Server();
- 
+
 server.connection({
-    host: host,
-    port: port
+  port: process.env.PORT || 5000
 });
- 
+
 server.register(githubWebhooksPlugin, function (err) {
   // Register github webhook auth strategy 
-  server.auth.strategy('githubwebhook', 'githubwebhook', { secret: token});
+  server.auth.strategy('githubwebhook', 'githubwebhook', {secret: token});
   // Apply the strategy to the route that handles webhooks 
   server.route([
     {
       method: 'POST',
       path: '/webhooks/github',
       config: {
-          auth: {
-              strategies: ["githubwebhook"],
-              payload: 'required'
-          }
+        auth: {
+          strategies: ['githubwebhook'],
+          payload: 'required'
+        }
       },
-      handler: function(request, reply) {
+      handler: function (request, reply) {
         // request.payload is the validated payload from GitHub
         console.log(request.payload);
         reply();
